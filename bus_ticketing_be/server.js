33946f995ebedf5da6adea127c08f40eaf6fdc75
kartheeswaran.rajasekaran <kartheeswaran.rajasekaran@abstract-tech.com>
2026-03-27 13:10:04 +0530

    RK-Changes in .env for port issue

--- bus_ticketing_be/server.js
+++ bus_ticketing_be/server.js
@@ -1,23 +1,24 @@
-require('dotenv').config();
-const app = require('./app');
-const connectDB = require('./config/db');
-const Ticket = require('./models/ticketModel');
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
+const Ticket = require("./models/ticketModel");
 
 connectDB();
 
 const initSeats = async () => {
-  const count = await Ticket.countDocuments();
-  if (count === 0) {
-    let seats = [];
-    for (let i = 1; i <= 40; i++) {
-      seats.push({ seatNumber: i });
-    }
-    await Ticket.insertMany(seats);
-    console.log("Seats initialized");
-  }
+	const count = await Ticket.countDocuments();
+	if (count === 0) {
+		let seats = [];
+		for (let i = 1; i <= 40; i++) {
+			seats.push({ seatNumber: i });
+		}
+		await Ticket.insertMany(seats);
+		console.log("Seats initialized");
+	}
 };
 
-app.listen(3000, async () => {
-  await initSeats();
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
 });
